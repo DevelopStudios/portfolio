f8084495b8231de638d762cf016e89ca27f19292
Charl <info@charlroux.co.za>
2020-07-09 19:52:45 +0200

    added toggle state to mobile menu

--- app.js
+++ app.js
@@ -1,83 +1,113 @@
-const http = require('http');
-const express = require('express');
-const sgMail = require('@sendgrid/mail');
-const hostname = '127.0.0.1';
-const port = 3000;
+require("dotenv").config();
+const http = require("http");
+const express = require("express");
 const app = express();
-let ejs = require('ejs');
+const parser = require("body-parser");
+const bodyParser = require("body-parser");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.API_KEY);
+let ejs = require("ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// create application/json parser
 
 let indexData = {
   projects: [
     {
-      name: 'BeyondBI',
-      desc: 'This is an Angular project built for marketing purposes and speed',
-      background: '/assets/beyond.png',
-      type: 'angular'
+      name: "BeyondBI",
+      desc: "This is an Angular project built for marketing purposes and speed",
+      background: "/assets/beyond.png",
+      type: "angular",
     },
     {
-      name: 'Portfolio',
-      desc: 'This is a nodejs and vanilla javascript project',
-      background: '/assets/portfolio.png',
-      type: 'nodejs'
-    }
+      name: "Portfolio",
+      desc: "This is a nodejs and vanilla javascript project",
+      background: "/assets/portfolio.png",
+      type: "nodejs",
+    },
   ],
-  service : [
+  service: [
     {
-      background: '/assets/web-hosting 1.jpg',
-      image: '/assets/icon.png',
-      header: 'Web Hosting',
-      text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nu pariatur excepteur sint occaecat.'
+      background: "/assets/web-hosting 1.jpg",
+      image: "/assets/icon.png",
+      header: "Web Hosting",
+      text:
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nu pariatur excepteur sint occaecat.",
     },
     {
-      background: '/assets/web-development 1.png',
-      image: '/assets/icon.png',
-      header: 'Web Developer',
-      text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nu pariatur excepteur sint occaecat.'
+      background: "/assets/web-development 1.png",
+      image: "/assets/icon.png",
+      header: "Web Developer",
+      text:
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nu pariatur excepteur sint occaecat.",
     },
     {
-      background: '/assets/Wordpress-development 1.png',
-      image: '/assets/icon.png',
-      header: 'Wordpress Developement',
-      text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nu pariatur excepteur sint occaecat.'
+      background: "/assets/Wordpress-development 1.png",
+      image: "/assets/icon.png",
+      header: "Wordpress Developement",
+      text:
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nu pariatur excepteur sint occaecat.",
     },
     {
-      background: '/assets/maintenance.jpg',
-      image: '/assets/icon.png',
-      header: 'Web Maintenance',
-      text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nu pariatur excepteur sint occaecat.'
-    }
+      background: "/assets/maintenance.jpg",
+      image: "/assets/icon.png",
+      header: "Web Maintenance",
+      text:
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nu pariatur excepteur sint occaecat.",
+    },
   ],
   experience: [
     {
-      date:'FEB 2016 - 2017',
-      title: 'Web Developer',
-      company: 'Vivant',
-      text: 'My duties included communicating to clients regarding web related queries, Wordpress Development and converting PSD files to working Wordpress Sites.'
+      date: "FEB 2016 - 2017",
+      title: "Web Developer",
+      company: "Vivant",
+      text:
+        "My duties included communicating to clients regarding web related queries, Wordpress Development and converting PSD files to working Wordpress Sites.",
     },
     {
-      date:'FEB 2018 - 2019',
-      title: 'Front End Developer',
-      company: 'BeyondBI',
-      text: 'My duties included Wordpress Developement, Wordpress Maintenance and Theme Developement, and Angular development using Typescript,SCSS,HTML, and Nodejs.'
+      date: "FEB 2018 - 2019",
+      title: "Front End Developer",
+      company: "BeyondBI",
+      text:
+        "My duties included Wordpress Developement, Wordpress Maintenance and Theme Developement, and Angular development using Typescript,SCSS,HTML, and Nodejs.",
     },
     {
-      date:' FEB 2019 - current',
-      title: 'Front-End Engineer',
-      company: 'TouchFoundry',
-      text: 'My role is to write and style the front-end components that meet the requirements of our mocks and full-fill our user stories. I also monitor and process pull requests for production deployments.'
-    }
-  ]
-}
+      date: " FEB 2019 - current",
+      title: "Front-End Engineer",
+      company: "TouchFoundry",
+      text:
+        "My role is to write and style the front-end components that meet the requirements of our mocks and full-fill our user stories. I also monitor and process pull requests for production deployments.",
+    },
+  ],
+};
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // index page
-app.get('/', function(req, res) {
-    res.render('pages/index',{indexData: indexData});
+app.get("/", function (req, res) {
+  res.render("pages/index", { indexData: indexData });
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.post("/", function (req, res) {
+  console.dir(req.body);
+  const msg = {
+    to: req.body.email,
+    from: "charlroux641@gmail.com",
+    subject: `Portfolio funnel ${req.body.name}`,
+    text: req.body.text,
+    html: "<p>This is a test tag</p>",
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Message sent");
+    })
+    .catch((error) => {
+      console.log(error.response.body);
+    });
+  res.redirect("/");
 });
+
+app.listen(3000, function () {});
--- public/js/custom.js
+++ public/js/custom.js
@@ -1,30 +1,46 @@
-
-$(function() {
-	//Sort functionality
-		var selectedClass = "";
-		$(".sort").click(function(){
-		selectedClass = $(this).attr("data-rel");
-		$( '.sort[ data-rel=' + selectedClass + ']' ).addClass( 'active' );
-		$( '.sort').not('[ data-rel=' + selectedClass + ']').removeClass('active');
+$(function () {
+  //Sort functionality
+  var selectedClass = "";
+  $(".sort").click(function () {
+    selectedClass = $(this).attr("data-rel");
+    $(".sort[ data-rel=" + selectedClass + "]").addClass("active");
+    $(".sort")
+      .not("[ data-rel=" + selectedClass + "]")
+      .removeClass("active");
     $("#portfolio").fadeTo(100, 0.1);
-		$("#portfolio div").not("."+selectedClass).fadeOut();
-    setTimeout(function() {
-      $("."+selectedClass).fadeIn();
+    $("#portfolio div")
+      .not("." + selectedClass)
+      .fadeOut();
+    setTimeout(function () {
+      $("." + selectedClass).fadeIn();
       $("#portfolio").fadeTo(500, 1);
     }, 500);
-	});
+  });
 
   // Show text overlay
-  $('.company-image-overlay').hide();
-  $('.tile').mouseover(handleHover).mouseout(handleOver);
+  $(".company-image-overlay").hide();
+  $(".tile").mouseover(handleHover).mouseout(handleOver);
   function handleHover() {
     let overlay = this.children[1];
-		let overlayText = this.children[1].children;
+    let overlayText = this.children[1].children;
     $(overlay).show();
-		$(overlayText).show();
+    $(overlayText).show();
   }
   function handleOver() {
     let overlay = this.children[1];
     $(overlay).hide();
+  }
+  let menuButton = $(".hamburger");
+  let mobileMenu = $(".mobile-menu");
+  mobileMenu.hide();
+  menuButton.click(toggleMobileMenu);
+  function toggleMobileMenu() {
+    if (menuButton.hasClass("is-active")) {
+      menuButton.removeClass("is-active");
+      mobileMenu.hide(500);
+    } else {
+      menuButton.addClass("is-active");
+      mobileMenu.show(500);
+    }
   }
 });
